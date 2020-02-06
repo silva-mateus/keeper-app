47da215d09caf578204e8029a7e36b0843ea8127
silva-mateus <46615092+silva-mateus@users.noreply.github.com>
2020-02-06 13:35:15 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Heading from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+
+function App(){
+    return (<div>
+        <Heading />
+        <Note />
+        <Footer />
+    </div>);
+}
+
+export default App;
---
+++ src/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+
+function Footer(){
+    return <footer>
+        <p>Copyright © {year}</p>
+    </footer>
+}
+
+export default Footer;
